58426507d32f8305e9e649507efd9a6b92c7a893
caiooliveirah09 <leelros@hotmail.com>
2022-08-17 19:54:29 -0300

    solving lint errors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import MyContext from './provider'
+import MyContext from './provider';
 
 ReactDOM.render(
   <MyContext>
     <App />
   </MyContext>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
