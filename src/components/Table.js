7475b53f21f96dc7281c73bb659f3f154aec41d8
caiooliveirah09 <leelros@hotmail.com>
2022-08-18 16:57:04 -0300

    i solved requirement 2

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,36 +1,58 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import MyContext from '../context';
 
 export default function Table() {
-  const { planets } = useContext(MyContext);
+  const { planets, keys } = useContext(MyContext);
+  const [filterByName, setFilterByName] = useState();
+  const [newPlanets, setNewPlanets] = useState([]);
+
+  useEffect(() => {
+    setNewPlanets(planets);
+  }, [planets]);
+
+  useEffect(() => {
+    if (planets) {
+      const filtered = planets.filter((p) => p.name.toLowerCase()
+        .includes(filterByName.toLowerCase()));
+      setNewPlanets(filtered);
+    }
+  }, [filterByName]);
+
   return (
-    <table>
-      <thead>
-        <tr>
-          { planets && Object.keys(planets[0]).map((keys) => (
-            <th key={ keys }>{ keys }</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        { planets && planets.map((planet) => (
-          <tr key={ planet.name }>
-            <td>{ planet.name }</td>
-            <td>{ planet.rotation_period }</td>
-            <td>{ planet.orbital_period }</td>
-            <td>{ planet.diameter }</td>
-            <td>{ planet.climate }</td>
-            <td>{ planet.gravity }</td>
-            <td>{ planet.terrain }</td>
-            <td>{ planet.surface_water }</td>
-            <td>{ planet.population }</td>
-            <td>{ planet.films }</td>
-            <td>{ planet.created }</td>
-            <td>{ planet.edited}</td>
-            <td>{ planet.url }</td>
+    <>
+      <input
+        data-testid="name-filter"
+        type="text"
+        onChange={ ({ target }) => { setFilterByName(target.value); } }
+      />
+      <table>
+        <thead>
+          <tr>
+            { keys && keys.map((key) => (
+              <th key={ key }>{ key }</th>
+            ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          { planets.length > 0 && newPlanets.map((p) => (
+            <tr key={ p.name }>
+              <td>{ p.name }</td>
+              <td>{ p.rotation_period }</td>
+              <td>{ p.orbital_period }</td>
+              <td>{ p.diameter }</td>
+              <td>{ p.climate }</td>
+              <td>{ p.gravity }</td>
+              <td>{ p.terrain }</td>
+              <td>{ p.surface_water }</td>
+              <td>{ p.population }</td>
+              <td>{ p.films }</td>
+              <td>{ p.created }</td>
+              <td>{ p.edited}</td>
+              <td>{ p.url }</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
--- src/provider/index.js
+++ src/provider/index.js
@@ -4,7 +4,8 @@ import MyContext from '../context';
 import Api from '../services/fetch';
 
 export default function MyProvider({ children }) {
-  const [planets, setPlanets] = useState();
+  const [planets, setPlanets] = useState([]);
+  const [keys, setKeys] = useState([]);
 
   useEffect(() => {
     async function fetch() {
@@ -12,12 +13,13 @@ export default function MyProvider({ children }) {
       const data = response.results;
       data.map((planet) => delete planet.residents);
       setPlanets(data);
+      setKeys(Object.keys(data[0]));
     }
     fetch();
   }, []);
 
   return (
-    <MyContext.Provider value={ { planets, setPlanets } }>
+    <MyContext.Provider value={ { planets, setPlanets, keys } }>
       {children}
     </MyContext.Provider>
   );
